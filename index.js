2e0ac95af9576f51f1b797ab49c948a7e4230bae
Francisco-Fortes <fortesfco@gmail.com>
2022-10-04 20:38:13 +0200

    Final commit

--- index.js
+++ index.js
@@ -25,10 +25,12 @@ let pepperYolkCheeseMix = smallAmountPepper + yolkCheeseMix;
 console.log(
   "Roast the black pepper and add a little amount of it to the yolk and cheese mixture and put aside"
 );
-
 let brownGuanciale = guanciale;
 console.log(
-  "Brown the strips of guanciale for 3 minutes at medium heat, then 1 minune on high heat til crisp, then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down"
+  "Brown the strips of guanciale for 3 minutes at medium heat, then 1 minute on high heat til crisp"
+);
+console.log(
+  "Then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down"
 );
 console.log(
   "Cook the pasta with a pinch of salt in the water (guanciale is already very salty)"
@@ -52,21 +54,29 @@ console.log(
   glassOfWater,
   "grams approx"
 );
-
-/* 
-WIP: finish it after debrief
+//a spoon contains 15 grams
+let spoon = 15;
 console.log(
-  "After the Spaghetti absorbed water you should have",
-  cookedSpaghetti,
-  "grams"
+  "Pour 2 spoons of the reserved hot water",
+  "(",
+  spoon * 2,
+  "grams approx) into the part of the frying pan containing the cooled guanciale and turn on the heat"
 );
-
-Pour 2 spoons of the reserved hot water into the part of the frying pan containing the cooled guanciale and turn on the heat:
-this will create a creamy sauce with the pasta starch contained in that water.
-TURN OFF THE HEAT AND SET ASIDE for 1 minute, then transfer the pasta to the same pan and mix together.
-Add the yolk and cheese mixture, stirring rapidly. In the warm pan with the hot pasta, the eggs will cook gently and become creamy.
-It is important to do this part quickly to prevent the yolks from congealing and taking on the texture of scrambled eggs.
-Add the remaining roasted black pepper and serve immediately.
-
-let carbonara; 
-*/
+console.log(
+  "   This will create a creamy sauce with the pasta starch contained in that water"
+);
+console.log(
+  "TURN OFF THE HEAT AND SET ASIDE for 1 minute, then transfer the pasta to the same pan and mix together"
+);
+let greasyPanMix = brownGuanciale + cookedSpaghettiWeight;
+console.log("Add the yolk and cheese mixture, stirring rapidly");
+let mainMix = greasyPanMix + pepperYolkCheeseMix;
+console.log(
+  "In the warm pan with the hot pasta, the eggs will cook gently and become creamy"
+);
+console.log(
+  "It is important to do this part quickly to prevent the yolks from congealing and taking on the texture of scrambled eggs"
+);
+console.log("Add the remaining roasted black pepper and serve immediately");
+let carbonara = mainMix + roastedPepper;
+console.log("Enjoy yours", carbonara, "grams of Carbonara");
